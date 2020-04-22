5850ad1cea1a5a7cf782e15686e1fac1e0d9d75e
David Giraldo <davidgiraldobueno@gmail.com>
2020-04-21 20:22:00 -0500

    Updates

---
+++ precache-manifest.44b0731c099bac9f448a6b2612dfa701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92039ddf9170b634d572df317bb37613",
+    "url": "/Covid-19-Cards/index.html"
+  },
+  {
+    "revision": "a39d5e8d61f47ef5fd40",
+    "url": "/Covid-19-Cards/static/css/main.4ad6be4f.chunk.css"
+  },
+  {
+    "revision": "ba7e86b49d6dbdf2b49f",
+    "url": "/Covid-19-Cards/static/js/2.25a60b36.chunk.js"
+  },
+  {
+    "revision": "348e18c767030a34e3e00df7559622cb",
+    "url": "/Covid-19-Cards/static/js/2.25a60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39d5e8d61f47ef5fd40",
+    "url": "/Covid-19-Cards/static/js/main.4f97fee3.chunk.js"
+  },
+  {
+    "revision": "c1d99ffd73778f8ca7b7",
+    "url": "/Covid-19-Cards/static/js/runtime-main.1c7ec447.js"
+  }
+]);
